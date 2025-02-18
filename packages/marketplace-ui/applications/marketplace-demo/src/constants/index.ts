fdd5d20c994f15e47240be22a06a88b456df9df0
privatejava <privatejava@yahoo.com>
2025-02-18 14:46:40 +0545

    updated code to use custom currency label.

--- packages/marketplace-ui/applications/marketplace-demo/src/constants/index.ts
+++ packages/marketplace-ui/applications/marketplace-demo/src/constants/index.ts
@@ -2,4 +2,4 @@ export const API_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_API_ENDPOINT as stri
 export const BUILD_ENDPOINT = process.env.NEXT_PUBLIC_GRAPHQL_BUILD_ENDPOINT as string
 export const BLOCKFROST_API_KEY = process.env.NEXT_PUBLIC_BLOCKFROST_API_KEY as string
 export const IPFS_GATEWAY_URL = process.env.NEXT_PUBLIC_IPFS_GATEWAY_URL as string
-export const TOKEN_LABEL = 'tAda'
+export const TOKEN_LABEL =  process.env.NEXT_PUBLIC_TOKEN_LABEL as string || 'tAda'
